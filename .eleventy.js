93d8a5713e7ad1a6f0abee9710a55f8fa0e133a1
stefanotugiuru <stefanomaurici77@gmail.com>
2026-06-28 20:11:45 +1000

    feat: add piatti detail pages with restaurant cross-link

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
+const ristoranti = require("./src/_data/ristoranti.json");
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addFilter("ristorantePerSlug", (slug) =>
+    ristoranti.find((r) => r.slug === slug)
+  );
 
   return {
     dir: {
