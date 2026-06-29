63e14fdaf7ee6c5009841dd79e28142a7a1d88e8
stefanotugiuru <stefanomaurici77@gmail.com>
2026-06-29 10:45:42 +1000

    chore: remove Eleventy implementation, scaffold Next.js project

--- src/_data/site.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  langs: ["it", "en"],
-  defaultLang: "it",
-  // Local preview value. Update to the real GitHub Pages / custom domain
-  // URL in Task 19, once the dedicated GitHub repo and domain exist.
-  baseUrl: "http://localhost:8080",
-};
